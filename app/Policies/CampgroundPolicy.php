40f79affcdde90b57e506318b7e49540d27d3cb2
shelbon <mouclepatrick@protonmail.com>
2022-09-04 15:29:11 +0200

    add  author relationship

--- app/Policies/CampgroundPolicy.php
+++ app/Policies/CampgroundPolicy.php
@@ -54,7 +54,7 @@ class CampgroundPolicy
      */
     public function update(?User $user, Campground $campground)
     {
-        return optional($user)->id === $campground->author
+        return optional($user)->id === $campground->author->id
             ? Response::allow()
             : Response::deny("you can't edit this campground,it's not your own.");
     }
@@ -68,7 +68,7 @@ class CampgroundPolicy
      */
     public function delete(?User $user, Campground $campground)
     {
-        return optional($user)->id === $campground->author
+        return optional($user)->id === $campground->author->_id
             ? Response::allow()
             : Response::deny("you can't delete this campground,it's not your own.");
     }
